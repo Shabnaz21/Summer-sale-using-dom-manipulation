2f4fb8468c9b6c809d4e15245d52d40b1e22b78c
shabnaz21 <shabnaz608@gmail.com>
2023-08-20 21:30:18 +0600

    compalte js work

--- cart.js
+++ cart.js
@@ -1,4 +1,6 @@
 let total = 0;
+let couponCode = '';
+let discount = 0;
 
 function addToCart(target) {
     const selectedItemContainer = document.getElementById('selected-item');
@@ -11,19 +13,61 @@ function addToCart(target) {
 
     const price = target.childNodes[3].childNodes[7].innerText.split(' ')[0];
     total += parseFloat(price);
-    const TotalPriceValue = (document.getElementById('Total Price').innerText = total);
-    document.getElementById('btn-confirm').addEventListener('keyup', function (event) {
-        const text = event.target.value;
-        const deleteButton = document.getElementById('btn-disable');
-        if (text == 'SELL200' || price < 200) {
-            deleteButton.removeAttribute('disabled');
-        }
-        else {
-            deleteButton.setAttribute('disabled', true)
-        }
-    })
-    if (TotalPriceValue == 0) {
-        deleteButton.removeAttribute('disabled');
+    document.getElementById('Total Price').innerText = total;
+    updateButtonState();  
+}
+
+// Enable or disable the button
+function updateButtonState() {
+    const purchaseButton = document.getElementById('btn-purchase');
+    const applyButton = document.getElementById('btn-disable');
+    const discountElement = document.getElementById('Discount price');
+    const totalElement = document.getElementById('total');
+
+    // Enable or disable the button based on the total price
+    if (total > 0) {
+        purchaseButton.removeAttribute('disabled');
+    } else {
+        purchaseButton.setAttribute('disabled', 'true'); 
+    }
+    
+    // Enable or disable the apply button based on the total price and coupon code
+    if (total >= 200 && couponCode === 'SELL200') {
+        applyButton.removeAttribute('disabled');
+        // Calculate discount and total cost
+        discount = total * 0.2;
+    } else {
+        applyButton.setAttribute('disabled', 'true');
+        discount = 0;
+    }
+    const totalCost = total - discount;
+
+    discountElement.innerText = discount.toFixed(2);
+    totalElement.innerText = totalCost.toFixed(2);
+}
+
+// Function to the coupon code
+document.getElementById('btn-confirm').addEventListener('input', function () {
+    couponCode = this.value;
+    updateButtonState();
+});
+
+// Reset all Data
+function resetPurchaseForm() {
+    document.getElementById('selected-item').innerHTML = '';
+    document.getElementById('Total Price').innerText = '';
+    document.getElementById('Discount price').innerText = '';
+    document.getElementById('total').innerText = '';
+
+    document.getElementById('btn-purchase').setAttribute('disabled', 'true');
+    document.getElementById('btn-disable').setAttribute('disabled', 'true');
+
+    document.getElementById('btn-confirm').value = '';
+
+    // Close the modal 
+    const modal = document.getElementById('my_modal_5');
+    if (modal.open) {
+        modal.close();
     }
 }
 
